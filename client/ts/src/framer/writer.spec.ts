8f90529c1811a62409125650f865f2d6bf486d9c
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-10-28 12:48:01 +0100

    [synnax] - streamlining advanced codec

--- client/ts/src/framer/writer.spec.ts
+++ client/ts/src/framer/writer.spec.ts
@@ -279,7 +279,7 @@ describe("Writer", () => {
     const values = Object.fromEntries(channelKeys.map((k) => [k, 1]));
     channelKeys.push(idx.key);
 
-    [false, true].forEach((reg) => {
+    [true, false].forEach((reg) => {
       let wStart = TimeStamp.now();
       it(
         `should write 100,000 frames - reg codec - ${reg}`,
@@ -294,7 +294,7 @@ describe("Writer", () => {
           });
           const streamer = await client.openStreamer({
             channels: channelKeys,
-            useExperimentalCodec: reg,
+            useExperimentalCodec: false,
           });
           const start = performance.now();
           try {
